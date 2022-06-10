7b383fc932074a6dd1d529d03c4b8267839438e0
omerpolat <omer_polat20@erdogan.edu.tr>
2022-06-10 22:36:58 +0300

    hw6

---
+++ ce100-hw6-algo-test-cpp/ce100-hw6-algo-test-cpp.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../ce100-hw6-algo-lib-cs/ce100-hw6-algo-lib-cpp.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ce100hw6algotestcpp
+{
+	TEST_CLASS(ce100hw6algotestcpp)
+	{
+	public:
+		
+		/*
+		TEST_METHOD(HOTP_TestMethod_1)
+		{
+
+			unsigned char fiK[20] = { 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b, 0x0b };
+			int C = 123;
+
+			int act = 132456;
+			int res = hotp(fiK, C);
+
+			Assert::AreEqual(act, res);
+
+		}*/
+	};
+}
